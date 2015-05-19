523b196718615c0a5c1435ea05118761c414f4c2
Blake Martinson <blake.martinson@gmail.com>
2015-05-19 19:08:55 -0400

    Add Vote

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519190310) do
+ActiveRecord::Schema.define(version: 20150519230819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20150519190310) do
     t.datetime "updated_at",     null: false
     t.string   "optimal_volume"
     t.integer  "artist_id"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "song_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
