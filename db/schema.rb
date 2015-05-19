66632589029214cf9949740df8e33c6e40716a8b
Blake Martinson <blake.martinson@gmail.com>
2015-05-19 15:04:41 -0400

    Class 2; Add artist_id to Song

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519174010) do
+ActiveRecord::Schema.define(version: 20150519190310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150519174010) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "optimal_volume"
+    t.integer  "artist_id"
   end
 
   create_table "votes", force: :cascade do |t|
