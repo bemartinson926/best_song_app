313da292719ce7634814b28ae0b5593784c12df5
Blake Martinson <blake.martinson@gmail.com>
2015-05-19 13:27:03 -0400

    Add current_hairstyle to Artist

--- db/migrate/20150519151943_create_artists.rb
+++ db/migrate/20150519151943_create_artists.rb
@@ -2,6 +2,7 @@ class CreateArtists < ActiveRecord::Migration
   def change
     create_table :artists do |t|
       t.string :full_name
+      t.string :current_hairstyle
 
       t.timestamps null: false
     end
